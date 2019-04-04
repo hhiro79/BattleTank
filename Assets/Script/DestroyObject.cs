80c6b9bc37bc870a36489811f7cf7910d76162ee
H.Hagiwara <hiroh79@gmail.com>
2019-04-05 04:46:36 +0900

    BattleTank（基礎／全31回）
    「6 オブジェクトに耐久力（HP）を加える」まで

---
+++ Assets/Script/DestroyObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    //エフェクトプレハブのデータを入れるための箱
+    public GameObject effectPrefab;
+
+    //2種類目のエフェクトを入れるための箱
+    public GameObject effectPrefab2;
+
+    public int objectHP;
+
+    //このメソッドはぶつかった瞬間に呼び出される
+    void OnTriggerEnter(Collider col){
+
+        //もしもぶつかった相手のTagにShellという名前が書いてあったならば
+        if(col.CompareTag("Shell")){
+
+            //オブジェクトのHPを1ずつ減らす
+            objectHP -= 1;
+
+            //もしもHPが0より大きい場合には
+            if(objectHP > 0){
+
+                //ぶつかってきたオブジェクトを破壊する
+                Destroy(col.gameObject);
+
+                //エフェクトを実体化する
+                GameObject effect = (GameObject)Instantiate(effectPrefab, transform.position, Quaternion.identity);
+
+                //エフェクトを2秒後に画面から消す
+                Destroy(effect, 2.0f);
+                
+            } else { //そうでない場合（HPが0以下になったら）
+                Destroy(col.gameObject);
+
+                //もう1種類のエフェクトを発生させる
+                GameObject effect2 = (GameObject)Instantiate(effectPrefab2, transform.position, Quaternion.identity);
+                Destroy(effect2, 2.0f);
+
+                //このスクリプトがついているオブジェクトを破壊する（thisは省略可）
+                Destroy(this.gameObject);
+
+            }
+        }
+    }
+}
---
+++ Assets/Script/ShotShell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotShell : MonoBehaviour
+{
+    public GameObject shellPrefab;
+    public float shotSpeed;
+    public AudioClip shotSound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //もしもSpaceキーを押したならば（条件）
+        //「Space」の部分を変更することで他のキーにすることができる
+        if(Input.GetKeyDown(KeyCode.Space)){
+
+            //砲弾のプレハブを実体化（インスタンス化）する
+            GameObject shell = Instantiate(shellPrefab, transform.position, Quaternion.identity) as GameObject;
+
+            //砲弾に付いているRigidbodyコンポーネントにアクセスする
+            Rigidbody shellRb = shell.GetComponent<Rigidbody>();
+
+            //forward(青軸＝Z軸)の方向に力を加える
+            shellRb.AddForce(transform.forward * shotSpeed);
+
+            //発射した砲弾を3秒後に破壊する
+            //（重要な考え方）不要になった砲弾はメモリ上から削除する
+            Destroy(shell, 3.0f);
+
+            //砲弾の発射音を出す
+            AudioSource.PlayClipAtPoint(shotSound, transform.position);
+        }
+    }
+}
