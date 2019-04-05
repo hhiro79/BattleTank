fe2398739b4568f7e52b01bf252e9b1e2286cbdd
H.Hagiwara <hiroh79@gmail.com>
2019-04-05 19:59:32 +0900

    attleTank（基礎／全31回）
    8 敵の攻撃を作る②（プレーヤーを感知して向きを変える）まで終了

---
+++ Assets/Script/EnemyShotShell.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotShell : MonoBehaviour
+{
+    public GameObject enemyShellPrefab;
+    public float shotSpeed;
+    public AudioClip shotSound;
+    private float shotInterval;
+
+    public float minShotCount;
+    public float maxShotCount;
+
+    private float randomValue;
+
+    void Start(){
+        randomValue = Random.Range(minShotCount, maxShotCount);
+
+        Debug.Log(randomValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shotInterval += 1;
+
+        if(shotInterval > randomValue){
+            GameObject enemyShell = (GameObject)Instantiate(enemyShellPrefab, transform.position, Quaternion.identity);
+
+            Rigidbody EnemyShotShellRb = enemyShell.GetComponent<Rigidbody>();
+
+            //forwardはZ軸方向（青軸）、この方向に力を加える
+            EnemyShotShellRb.AddForce(transform.forward * shotSpeed);
+
+            AudioSource.PlayClipAtPoint(shotSound, transform.position);
+
+            Destroy(enemyShell, 3.0f);
+
+            shotInterval = 0;
+            randomValue = Random.Range(minShotCount, maxShotCount);
+            Debug.Log(randomValue);
+        }
+    }
+}
---
+++ Assets/Script/Radar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    public Transform target;
+
+    //「OnTriggferStay」はトリガーが他のコライダーに触れている間中実行されるメソッド
+    void OnTriggerStay(Collider Col){
+
+        //もしも他のオブジェクトに「Player」というTagが付いていたならば
+        if(Col.CompareTag("Player")){
+
+            //「root」を使うと「親（最上位の親）」の情報を得ることができる
+            //LookAt()メソッドは指定した方向にオブジェクトの向きを回転させる
+            transform.root.LookAt(target);
+            Debug.Log(target);
+        }
+    }
+}
